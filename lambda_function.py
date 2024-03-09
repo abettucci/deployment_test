5cfde4a72ac6bca6befdadf978b091d0cf3e5e7a
abettucci <bettucci.agustin@gmail.com>
2024-03-08 23:21:18 -0300

    lambda test

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,10 @@
-from lib1 import sum
+# from lib1 import sum
 import json
 
 def lambda_handler(event, context):
-    print('hola')
-    try:
-        result = sum(5,4)
-        print('hola')
-        return {
-            "statusCode" : 200,
-            "body" : "hi " + result
-        }
+    result = sum(5,4)
 
-    except Exception as e:
-        print(e)
+    return {
+        "statusCode" : 200,
+        "body" : "hi " + result
+    }
