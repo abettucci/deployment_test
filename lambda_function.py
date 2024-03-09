beba6fc3cd9182e2d8078ee3f8dbf92abb179100
abettucci <bettucci.agustin@gmail.com>
2024-03-08 23:18:44 -0300

    lambda test

--- lambda_function.py
+++ lambda_function.py
@@ -2,9 +2,10 @@ from lib1 import sum
 import json
 
 def lambda_handler(event, context):
+    print('hola')
     try:
         result = sum(5,4)
-
+        print('hola')
         return {
             "statusCode" : 200,
             "body" : "hi " + result
